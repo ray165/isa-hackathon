1fd9bd70cce6f2ab3b0ccdee1616ee9c0b7e8918
Joseph Chan <josephchan36@gmail.com>
2023-10-31 09:28:45 -0700

    ai recieiving

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from transformers import pipeline
-from transformers import pipeline
+import json
+
 sentiment_pipeline = pipeline("sentiment-analysis")
 app = Flask(__name__)
 
@@ -10,8 +11,11 @@ def index():
 
 @app.route('/api')
 def analyse():
-    sentence = request.args.get('sentence')
-    result = sentiment_pipeline(sentence)
-    return(result)
+    print(request)
+    sentence = request.args.get('setence')
+    print(sentence)
+    result = json.dumps(sentiment_pipeline(sentence))  # Use json.dumps() to convert to JSON string
+    print(result)
+    return result, 200, {'Content-Type': 'application/json'}  # Set content type to JSON
 
 app.run()
