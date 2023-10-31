d3da2812f91b577c3528f1dbfd26e0c0fbf513b7
Joseph Chan <josephchan36@gmail.com>
2023-10-31 09:05:42 -0700

    added sentiment

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request
 from transformers import pipeline
-
+from transformers import pipeline
+sentiment_pipeline = pipeline("sentiment-analysis")
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return open('index.html').read()
 
 @app.route('/api')
 def analyse():
-    # sentence = request.args.get('sentence')
-    # sentiment_pipeline = pipeline("sentiment-analysis")
-    # data = ["I love you", "I hate you"]
-    # sentiment_pipeline(data)
+    sentence = request.args.get('sentence')
+    result = sentiment_pipeline(sentence)
+    sentiment_pipeline(data)
     return(sentence)
 
 app.run()
